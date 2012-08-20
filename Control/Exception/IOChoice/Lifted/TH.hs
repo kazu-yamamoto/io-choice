8189cbe53c1202e047ab059b50a1e556d9f5a97d
Kazu Yamamoto <kazu@iij.ad.jp>
2012-08-20 14:14:43 +0900

    adding doc.

--- Control/Exception/IOChoice/Lifted/TH.hs
+++ Control/Exception/IOChoice/Lifted/TH.hs
@@ -1,9 +1,28 @@
 {-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
+
+-- |
+-- This package provides a function to generate a choice operator
+-- in lifted IO monad by specifying exceptions to be caught.
+
 module Control.Exception.IOChoice.Lifted.TH (newIOChoice) where
+
 import Control.Exception.Lifted
 import Language.Haskell.TH
 
 import Control.Exception.IOChoice.THUtil
+
+-- |
+-- A function to generate a choice operator in lifted IO monad.
+-- 'IOException' is automatically added to specified exceptions.
+-- So, 'Control.Exception.IOChoice.Lifted.goNext' can be used with
+-- the new operator.
+--
+-- > {-# LANGUAGE TemplateHaskell #-}
+-- > import Control.Exception
+-- > import Control.Exception.IOChoice.Lifted.TH
+-- >
+-- > (||>>) :: MonadBaseControl IO m => m a -> m a -> m a
+-- > (||>>) = $(newIOChoice [''ErrorCall, ''ArithException])
 
 newIOChoice :: [Name] -> ExpQ
 newIOChoice =  newChoice [| catches |] [| Handler |]
--- Control/Exception/IOChoice/TH.hs
+++ Control/Exception/IOChoice/TH.hs
@@ -1,9 +1,28 @@
 {-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
+
+-- |
+-- This package provides a function to generate a choice operator
+-- in IO monad by specifying exceptions to be caught.
+
 module Control.Exception.IOChoice.TH (newIOChoice) where
+
 import Control.Exception
 import Language.Haskell.TH
 
 import Control.Exception.IOChoice.THUtil
+
+-- |
+-- A function to generate a choice operator in IO monad.
+-- 'IOException' is automatically added to specified exceptions.
+-- So, 'Control.Exception.IOChoice.goNext' can be used with
+-- the new operator.
+--
+-- > {-# LANGUAGE TemplateHaskell #-}
+-- > import Control.Exception
+-- > import Control.Exception.IOChoice.TH
+-- >
+-- > (||>>) :: IO a -> IO a -> IO a
+-- > (||>>) = $(newIOChoice [''ErrorCall, ''ArithException])
 
 newIOChoice :: [Name] -> ExpQ
 newIOChoice =  newChoice [| catches |] [| Handler |]
